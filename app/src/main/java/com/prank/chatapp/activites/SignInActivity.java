8885fa50cb3dfcac7abd4932560ac80e6b312677
Beka <40782830+beketovekb@users.noreply.github.com>
2021-12-04 22:29:30 +0600

    1%

--- app/src/main/java/com/prank/chatapp/activites/SignInActivity.java
+++ app/src/main/java/com/prank/chatapp/activites/SignInActivity.java
@@ -2,15 +2,28 @@ package com.prank.chatapp.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.prank.chatapp.R;
+import com.prank.chatapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+    }
+
+    private void setListener()
+    {
+        binding.textCreateNewAccount.setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/prank/chatapp/activites/SignUpActivity.java
+++ app/src/main/java/com/prank/chatapp/activites/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.prank.chatapp.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.prank.chatapp.R;
+import com.prank.chatapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners()
+    {
+        binding.textSignIn.setOnClickListener(view ->
+                onBackPressed());
     }
 }
