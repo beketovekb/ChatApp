d1faf9175468102a39b0c60a78fc9cf94aa7d435
Beka <40782830+beketovekb@users.noreply.github.com>
2021-12-06 17:36:42 +0600

    2%

--- app/src/main/java/com/prank/chatapp/activites/SignInActivity.java
+++ app/src/main/java/com/prank/chatapp/activites/SignInActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.prank.chatapp.R;
 import com.prank.chatapp.databinding.ActivitySignInBinding;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -25,5 +29,22 @@ public class SignInActivity extends AppCompatActivity {
     {
         binding.textCreateNewAccount.setOnClickListener(view ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+        //binding.buttonSignIn.setOnClickListener(view -> addDataToFirestore());
     }
+
+    /*private void addDataToFirestore()
+    {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Chirag");
+        data.put("last_name", "Kachhadiya");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }*/
 }
